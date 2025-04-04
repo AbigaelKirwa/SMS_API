84dd1ff00b1e2f164038d90de78c2898a7b84192
AbigaelKirwa <abigaelkirwa1937@gmail.com>
2025-04-04 08:06:32 +0000

    feat:adds celerty task to handle sending the sms to the api endpoint and attaching a payload

--- sms.py
+++ sms.py
@@ -30,4 +30,42 @@ SMS_API_ENDPOINT = os.getenv('SMS_API_ENPOINT', '')
 SMS_API_KEY = os.getenv('SMS_API_KEY', '')
 SMS_SENDER_ID = os.getenv('SMS_SENDER_ID', '')
 
+@celery.task
+def send_sms_task(phone_number, message, provider_endpoint= None):
+    # Celery task to send an SMS to the specified phone number
+    try:
+        # use provider-specific enpoint if provided otherwise use default
+        endpoint = provider_endpoint if provider_endpoint else SMS_API_ENDPOINT
+
+        if not endpoint:
+            return {
+                "status":500,
+                "response":"No SMS Provider enpoint provided",
+                "success":False
+            }
+
+        # this gives the payload structure of how data is sent to the API endpoint
+        payload = {
+            "api_key": SMS_API_ENDPOINT,
+            "to": phone_number,
+            "message": message,
+            "sender_id": SMS_SENDER_ID
+        }
+
+        # fetch the response after sending the payload
+        response = requests.post(endpoint, json=payload)
+
+        return{
+            "status":response.status_code,
+            "response":response.text,
+            "success":response.ok,
+            "phone_number":phone_number
+        }
+    except Exception as e:
+        return{
+            "status":500,
+            "response":str(e),
+            "success":False,
+            "phone_number":phone_number
+        }
 
